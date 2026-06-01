6a903c15714f4f7854ab1db5351e7d91dea8202d
engciiltire <mnuur2196@gmail.com>
2026-05-31 20:32:35 -0700

    all javascrip files

---
+++ exercise two/main.js
@@ -0,0 +1,21 @@
+
+
+let a = 15;
+let b= "15";
+
+// equal
+console.log(a ==b);
+//not equal 
+console.log(a !=b);
+// 	Strictly equal to 
+console.log(a===b);
+//Strictly not equal to 
+console.log(a!==b)
+//Greater than 
+console.log(a>b)
+//Greater than or equal
+console.log(a>=b)
+//	Less  than 
+console.log(a<b)
+//	Less  than or equal
+console.log(a<=b)
