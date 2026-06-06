f72b476008450039d1d3ed3feb4f1974cf2e13c6
engciiltire <mnuur2196@gmail.com>
2026-06-06 09:48:04 -0700

    exercise 10

---
+++ exercise-10/main.js
@@ -0,0 +1,8 @@
+
+let library =[
+    {title: "Theo of Golden: A Novel", author: "by Allen Levi", year: 2026  },
+    {title: "Desk & Deception", author: "by Wren Shaw ", year: 2019  }
+]
+
+console.log(library[0].title)
+console.log(library[1].author)
