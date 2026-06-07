f7b6acbd38a8c336f4311a489a785bd1d1c6826d
engciiltire <mnuur2196@gmail.com>
2026-06-07 10:31:45 -0700

    xercise waaye 15

---
+++ exercise-15/main.js
@@ -0,0 +1,11 @@
+let students=[
+    {name: "aadan", age: 30, class: "c32"},
+    {name: "mohamed", age: 24, class: "c2"},
+    {name: "yaxye aadan", age: 23, class: "c12"},
+    {name: "abdi", age: 19, class: "a21"}
+
+]
+ for (const student in students) {
+    console.log(students[student.length])
+        
+    }
