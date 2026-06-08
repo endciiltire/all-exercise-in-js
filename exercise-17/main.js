fb9ccd3bc1f4349037cfed1556cd4b06c63c9e1e
engciiltire <mnuur2196@gmail.com>
2026-06-08 01:41:47 -0700

    kan ku dar

---
+++ exercise-17/main.js
@@ -0,0 +1,15 @@
+
+let temperature = 26;
+
+if(temperature<0){
+    console.log("Very cold")
+}
+else if (temperature<15){
+    console.log("Cold")
+}
+else if (temperature <=25){
+    console.log("Warm")
+}
+else{
+    console.log("hot")
+}
---
+++ exercise-18/main.js
@@ -0,0 +1,5 @@
+
+let x =69;
+
+const marks=   x>=60? "pass": "fail"
+console.log("this student is "+marks)
