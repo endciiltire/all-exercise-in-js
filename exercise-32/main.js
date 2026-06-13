5f08f87d376a739b736673edc6e7ee52553987e0
engciiltire <mnuur2196@gmail.com>
2026-06-13 09:07:28 -0700

    commit of tood

---
+++ exercise-32/main.js
@@ -0,0 +1,9 @@
+
+const wiil= document.querySelector('h1');
+console.log(wiil)
+
+const Tood=document.querySelectorAll('.tood');
+console.log(Tood);
+
+const BUTTON = document.querySelectorAll('button')
+console.log(BUTTON);
